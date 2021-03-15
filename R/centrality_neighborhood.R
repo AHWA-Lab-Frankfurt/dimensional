be42a06c97d9ac1a519f1b695a55c858b2e75dcb
Nick Gestrich <gestrich@uni-frankfurt.de>
2021-03-15 21:16:23 +0100

    intial commit

---
+++ R/centrality_neighborhood.R
@@ -0,0 +1,46 @@
+#' Neighborhood centrality
+#'
+#' Calculate neighborhood centrality for a set of acotrs over a set of layers
+#' @param graph A graph of class tbl_graph
+#' @param actors An actor or a vector of actors. Defaults to all actors in the graph.
+#' @param layers A layer or a vector of layers. Defaults to all layers of the graph
+#' @export
+
+centrality_neighborhood <- function(graph, actors, layers){
+  if (missing(actors) & missing(layers)) {
+    x <- graph %>%
+      morph(to_simple) %>%
+      activate(nodes) %>%
+      mutate(neighbours = centrality_degree()) %>%
+      unmorph()
+    return(x)
+  } else if (missing(actors) & !missing(layers)){
+    y <- graph %>%
+      activate(edges) %>%
+      filter(name == layers) %>%
+      morph(to_simple) %>%
+      activate(nodes) %>%
+      mutate(neighbours = centrality_degree()) %>%
+      unmorph()
+    return(y)
+  } else if (!missing(actors) & missing(layers)){
+    z <- graph %>%
+      activate(nodes) %>%
+      filter(id == actors) %>%
+      morph(to_simple) %>%
+      activate(nodes) %>%
+      mutate(neighbours = centrality_degree()) %>%
+      unmorph()
+    return(z)
+  } else{
+    graph %>%
+      activate(edges) %>%
+      filter(name == layers) %>%
+      activate(nodes) %>%
+      filter(id == actors) %>%
+      morph(to_simple) %>%
+      activate(nodes) %>%
+      mutate(neighbours = centrality_degree()) %>%
+      unmorph()
+  }
+}
