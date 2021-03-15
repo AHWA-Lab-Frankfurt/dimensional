fa68d708a19b3556bb7c0d1879fb17d594890ff6
Nick Gestrich <gestrich@uni-frankfurt.de>
2021-03-15 21:16:51 +0100

    update documentation

--- R/flatten.R
+++ R/flatten.R
@@ -1,4 +1,8 @@
+#' Flatten a multilayer network
+#'
 #' A function to flatten a multilayer network into a single-layer network, see Dickison et al. 2016, p.39
+#' @param graph A graph of the tbl-graph class
+#' @param weight A weight attribute for weighted edges. Defaults to unweighted edges.
 #' @export
 flatten <- function(graph, weight){
   weight = rlang::enquo(weight)
