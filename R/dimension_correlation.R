ff78f281b8258316a745333571fef0b0175362d7
JuanMArcops <jmpuertaschardt@outlook.de>
2021-03-26 19:30:20 +0100

    minor changes in matrixes

--- R/dimension_correlation.R
+++ R/dimension_correlation.R
@@ -5,60 +5,52 @@
 #' @param weighted Logical value if weight of the edges of the dimensions is considered or not. Defaults to unweighted network.
 
 dimension_correlation <- function(graph, weighted = FALSE){
+
+  edges.name <- graph %>%
+    get.data.frame("edges") %>%
+    select(name) %>%
+    deframe() %>%
+    unique()
+
+  matlist <- list()
+  cor.mat <- c()
+
   if(weighted == TRUE){
 
-    edges.name <- graph %>%
-      get.data.frame("edges") %>%
-      select(name) %>%
-      deframe() %>%
-      unique()
-
-    matlist <- list()
-
-    for(j in 1:length(edges.name)) {
+       for(j in 1:length(edges.name)) {
       mat <- decor.graph %>%
         activate(edges) %>%
         filter(name == edges.name[j]) %>%
-        as_adjacency_matrix(attr = "weight") %>%
-        as.matrix() %>%
-        as.vector()
+        as_adjacency_matrix(attr = "weight")
+
+      mat[lower.tri(mat, diag = TRUE)] <- NA
+      mat <- mat %>%
+        as.vector()%>%
+        na.omit()
 
       matlist[[edges.name[j]]] <- mat
-      cor.mat <- c()
+
+    }
     }
 
-    for(i in 1:length(matlist)){
-      for(j in 1:length(matlist)){
-        cor <- cor(matlist[[i]], matlist[[j]])
-        cor.mat <- c(cor.mat, cor)
-      }
-    }
 
-    dimension.correlation <- matrix(cor.mat, nrow = length(matlist), ncol = length(matlist))
-    colnames(dimension.correlation) <- edges.name
-    rownames(dimension.correlation) <- edges.name
-  }
   else{
-    edges.name <- graph %>%
-      get.data.frame("edges") %>%
-      select(name) %>%
-      deframe() %>%
-      unique()
-
-    matlist <- list()
 
     for(j in 1:length(edges.name)) {
       mat <- decor.graph %>%
         activate(edges) %>%
         filter(name == edges.name[j]) %>%
-        as_adjacency_matrix() %>%
-        as.matrix() %>%
-        as.vector()
+        as_adjacency_matrix()
+
+      mat[lower.tri(mat, diag = TRUE)] <- NA
+      mat <- mat %>%
+        as.vector()%>%
+        na.omit()
 
       matlist[[edges.name[j]]] <- mat
-      cor.mat <- c()
-    }
 
+    }
+}
     for(i in 1:length(matlist)){
       for(j in 1:length(matlist)){
         cor <- cor(matlist[[i]], matlist[[j]])
@@ -70,6 +62,6 @@ dimension_correlation <- function(graph, weighted = FALSE){
     colnames(dimension.correlation) <- edges.name
     rownames(dimension.correlation) <- edges.name
 
-  }
+
   return(dimension.correlation)
 }
