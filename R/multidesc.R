c7a4464c4e00a5b3f79eaca2c6da774459facd5d
Nick Gestrich <gestrich@uni-frankfurt.de>
2022-02-17 10:37:14 +0100

    update compare_layers fn

--- R/multidesc.R
+++ R/multidesc.R
@@ -2,16 +2,17 @@
 #' @import tidygraph
 #' @importFrom rlang .data
 #' @export
-multidesc <- function(graph){
-  x <- flatten(graph)
+compare_layers <- function(graph, var){
+  x <- flatten(graph, weighted = TRUE)
   order <- igraph::gorder(x)
   density <- igraph::graph.density(x)
   cc <- igraph::transitivity(x, type = "global")
   degcent <- igraph::centralization.degree(x)
   avgpath <- igraph::average.path.length(x, directed = FALSE)
-  diameter <- igraph::diameter(x, directed = FALSE, weights = igraph::E(x)$weight_sum)
+  diameter <- igraph::diameter(x, directed = FALSE, weights = igraph::E(x)$weight)
 
-  flattened <- tibble::tibble(order, density, cc, degcent$centralization, avgpath, diameter)
+  flattened <- tibble::tibble(order, density, cc, degcent$centralization, avgpath, diameter) %>%
+    mutate(layer = "flat graph", .before = 1)
 
 
   layerwise.desc <- function(layer){
@@ -20,28 +21,27 @@ multidesc <- function(graph){
 
     x <- graph %>%
       activate(edges) %>%
-      filter(name == layername)
+      filter({{var}} == layername) %>%
+      activate(nodes) %>%
+      filter(!node_is_isolated())
 
     order <- igraph::gorder(x)
     density <- igraph::graph.density(x)
     cc <- igraph::transitivity(x, type = "global")
     degcent <- igraph::centralization.degree(x)
     avgpath <- igraph::average.path.length(x, directed = FALSE)
-    diameter <- igraph::diameter(x, directed = FALSE, weights = E(x)$weight_sum)
+    diameter <- igraph::diameter(x, directed = FALSE, weights = igraph::E(x)$weight)
 
-    layername <- tibble::tibble(order, density, cc, degcent$centralization, avgpath, diameter)
+    layername <- tibble::tibble(order, density, cc, degcent$centralization, avgpath, diameter) %>%
+      mutate(layer = {{layer}}, .before = 1)
   }
 
   #make a list of layers
-  layerlist <- graph %>% activate(edges) %>% as_tibble() %>% distinct(name) %>% pull(name) %>% as.list()
-  layerwise <- lapply(layerlist, layerwise.desc)
+  layerlist <- graph %>% activate(edges) %>% as_tibble() %>% distinct({{var}}) %>% pull({{var}}) %>% as.list()
+  layerwise <- purrr::map(layerlist, layerwise.desc)
   layers <- layerwise %>% purrr::reduce(full_join)
   desc <- dplyr::bind_rows(flattened, layers)
 
   return(desc)
 
 }
-
-#to do on this function:
-# 1) we need names on the final table in order to know which dimensions the rows represent
-# 2) there is something weird about the results from average path length and transitivity. Need to check that we are using them correctly. What about weighting these cases?
